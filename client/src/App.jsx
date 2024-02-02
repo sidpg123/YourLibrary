fa1050eae5b21bb5616dc7029a8ee248a793028f
Siddharth <preparation013@gmail.com>
2024-02-02 22:21:22 +0530

    updated a typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import {
 	Routes,
 	Navigate
 } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button({onClick, label}) {
   return (
-    <button disabled={isSubmitting} type="submit" onClick={onClick} className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{isSubmitting ? "Loading..." : {label}}</button>
+    <button type="submit" onClick={onClick} className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{label}</button>
   )
 }
