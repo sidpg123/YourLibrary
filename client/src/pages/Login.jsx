4ddf4cdd3e6a770771adb4f8036fb5728d763f3a
Siddharth <preparation013@gmail.com>
2024-02-03 01:00:31 +0530

    Sign Up page is recreated using antd. Working properly. Need to so styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,101 +1,79 @@
+import { Form, Button, Input } from 'antd';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+
 import React from 'react'
-import library from '../assets/kitlib.png'
-import Heading from '../components/Heading'
-import SubHeading from '../components/SubHeading'
-import InputBox from '../components/InputBox'
-import { useState } from 'react'
-import { useNavigate } from "react-router-dom"
-import Button from '../components/Button'
-import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { zod } from "zod";
-import { SubmitHandler, useForm } from 'react-hook-form';
-
-const signupBody = zod.object({
-  username: zod.string().email(),
-  password: zod.string().min(6),
-  firstName: zod.string(),
-  lastName: zod.string()
-})
-
-
 
 export default function Login() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
-  } = useForm({
-    defaultValues: {
-      email: "test@email.com",
-    },
-    resolver: zodResolver(signupBody),
-  });
 
-  const onSubmit =async () => {
+  const onFinish = async (values) => {
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/user/signup', {
-        firstName,
-        lastName,
-        username,
-        password
-      });
+      const response = await axios.post('http://localhost:3000/api/v1/user/signup', values);
 
-      //console.log("Response data:", response.data); // Log the response data to the console
-
-      const token = `Bearer ${response.data.token}`;
-
-      if (token) {
+      if (response.data.token) {
+        const token = response.data.token;
+        toast.success(response?.data.message);
         localStorage.setItem("token", token);
         navigate("/dashboard");
       } else {
-        alert("Token is missing in the response. Please try again.");
+        toast.error(response?.data.message);
       }
-    } catch (error) {
-      console.error("Error during sign up:", error);
-      alert("Invalid credentials. Please try again.");
+    } catch (err) {
+      if (err.response?.data.message) toast.error(err.response?.data.message);
+      else toast.error("Something went wrong!");
+      console.log(err);
     }
-  } 
+  }
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 justify-center'>
-      <div className=' bg-orange-100 flex flex-col justify-center'>
-        <form onSubmit={handleSubmit(onSubmit)} className=' flex flex-col justify-center max-w-[400px] w-full rounded-lg bg-slate-100 mx-auto p-6 '>
-          <Heading label={"Sign Up"} />
-          <SubHeading label={"Enter your information to create your account"} />
-
-          <InputBox register={register("firstName")}  label={"First Name"} onChange={e => {
-            setFirstName(e.target.value)
-          }} paceholder={"First Name"} />
-
-          <InputBox register={register("lastName")} label={"Last Name"} onChange={e => {
-            setLastName(e.target.value)
-          }} paceholder={"Last Name"} />
-
-          <InputBox register={register("username")} label={"Email-id"} onChange={e => {
-            setUsername(e.target.value)
-          }} paceholder={"Email-id"} />
-
-          <InputBox register={register("password")} label={"Password"} onChange={e => {
-            setPassword(e.target.value)
-          }} paceholder={"Set Passowrd"} />
-
-          <div className='flex justify-center mt-5 '>
-            <Button label={"Sign Up"}></Button>
-          </div>
-
-        </form>
-      </div>
+    <div>
       <div>
-        <img className=' h-screen w-screen ' src={library} />
+        <Form layout='vertical' onFinish={onFinish} autoComplete='off'>
+
+          <Form.Item name='firstName' label='First Name' rules={[
+            {
+              required: true,
+              message: "Please enter your name",
+            },
+            { whitespace: true },
+            { min: 3 },
+          ]}
+            hasFeedback>
+            <Input placeholder='Type your first name.' />
+          </Form.Item>
+
+          <Form.Item name='lastName' label='Last Name' hasFeedback >
+            <Input placeholder='Type your first name.' />
+          </Form.Item>
+
+          <Form.Item
+            name="username"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+              { type: "email", message: "Please enter a valid email" },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Type your email" />
+          </Form.Item>
+
+          <Form.Item label="Password" name="password" required>
+            <Input.Password placeholder='Password' />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ span: 24 }}>
+            <Button block type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+
+        </Form>
       </div>
     </div>
   )
-}   
+}
