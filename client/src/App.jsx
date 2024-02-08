6e043ace154ea831c7346f03423be18f17077077
Siddharth <preparation013@gmail.com>
2024-02-08 19:53:14 +0530

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
-import jwt from "jsonwebtoken";
-import JWT_SECRET from './config';  // Adjust the path based on your project structure
-
+//import jwt from "jsonwebtoken";
+//import { JWT_SECRET } from "./config";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Signin from "./pages/Signin";
 import LibrDashboard from "./pages/LibrDashboard";
 import UserDashboard from "./pages/UserDashboard";
 
+
 function App() {
-  const location = useLocation();
+  // const location = useLocation();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
+  // useEffect(() => {
+  //   const token = localStorage.getItem('token');
 
-    if (token) {
-      try {
-        const decodedToken = jwt.verify(token, JWT_SECRET);
-        const role = decodedToken.role;
+  //   if (token) {
+  //     try {
+  //       const decoded = jwt.verify(token, JWT_SECRET);
+  //       const role = decoded.role;
 
-        if (role === 'user' && location.pathname === '/') {
-          Navigate('/userdashboard', { replace: true });
-        } else if (role === 'librarian' && location.pathname === '/') {
-          Navigate('/librarianDashboard', { replace: true });
-        }
-      } catch (error) {
-        console.error('Error decoding token:', error);
-        // Handle the error, e.g., log it or redirect to a login page
-      }
-    }
-  }, [location.pathname]);
+  //       if (role === 'user' && location.pathname === '/') {
+  //         navigate('/userdashboard', { replace: true });
+  //       } else if (role === 'librarian' && location.pathname === '/') {
+  //         navigate('/librarianDashboard', { replace: true });
+  //       }
+  //     } catch (error) {
+  //       console.error('Error decoding token:', error);
+  //     }
+  //   }
+  // }, [location.pathname, navigate]);
 
   return (
     <>
